59bc88816615d96bf90e55f8ca8bb5cd58f02140
Dang Nguyen <dangnt0490@gmail.com>
2018-02-06 15:44:50 +0700

    index file goc

---
+++ index.php
@@ -0,0 +1,37 @@
+<!--<!DOCTYPE html>-->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="Cache-Control" content="public"/>
+        <meta http-equiv="content-type" content="application/vnd.hbbtv.xhtml+xml; charset=UTF-8"/>
+        <title>Book Hotel Ninh Binh</title>
+        <link type="text/css" href="maincss.css" rel="stylesheet">
+
+        <script type="text/javascript" src="keycodes.js"></script>
+        <script type="text/javascript" src="event.js"></script>
+        <script src="js/eventCalendar.js" type="text/javascript"></script>
+        <script src="js/eventKeyboard.js" type="text/javascript"></script>
+        <link href="css/keyboard.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+
+        <link href="css/calendar.css" rel="stylesheet" type="text/css"/>
+
+        <script type="text/javascript">
+            //<![CDATA[
+            window.onload = function () {
+
+                onLoad();
+
+                document.onkeypress = keyEvent;
+            };
+            //]]>
+        </script>
+    </head>
+    <body>
+        <object id="oipfAppMan" type="application/oipfApplicationManager" style="visibility: hidden; display: none;"></object>
+       
+
+
+
+    </body>
+</html>
